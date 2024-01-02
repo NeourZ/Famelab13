63089a4c0008162d74b7b05460d3db697772ae70
Fame <pattanitfame@gmail.com>
2024-01-02 17:30:16 +0700

    lab13_2 completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,32 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void updateImage(bool image[][M],int s,int x,int y){
+    for (int i = 0 ; i < N ; i++){
+        for (int j = 0 ; j < M ; j++){
+            double f = pow((i - x),2) ;
+            double b = pow((j - y),2) ;
+            if (sqrt(f+b) <= (s - 1)){
+                image[i][j] = true ;
+            }
+            else {}
+        }
+    }
+}
+
+void showImage(const bool image[][M]){
+    cout << "------------------------------------------------------------------------\n" ;
+    for (int i = 0 ; i < N ; i++){
+        cout << "|" ;
+        for (int j = 0 ; j < M ; j++){
+            if (image[i][j] == 1){
+                cout << '*' ;
+            }
+            else{
+                cout << ' ' ;
+            }
+        }
+        cout << "|\n" ;
+    }
+    cout << "------------------------------------------------------------------------\n" ;
+}
